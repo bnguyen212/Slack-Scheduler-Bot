b8753edebc21f4ef1ddad562e3a2b8623dafa347
fxm84625 <frankyxm@hotmail.com>
2018-03-27 16:30:33 -0700

    Api.AI integration errorfix v5.2

--- routes/routes.js
+++ routes/routes.js
@@ -40,7 +40,7 @@ rtm.on( 'message', ( event ) => {
     // Give Message to Api AI
     fetch( 'https://api.dialogflow.com/v1/query?v=20150910', {
         method: 'POST',
-        headers: { Bearer: API_AI_DEV_TOKEN, "Content-Type": "application/json" },
+        headers: { "bearer": API_AI_DEV_TOKEN, "content-type": "application/json" },
         body: {
             sessionId: "ai84625",
             lang: 'en',
