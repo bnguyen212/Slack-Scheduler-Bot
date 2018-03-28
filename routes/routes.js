962e1cb8ba385b0e77ccc3a96e3498bf3bd46c81
fxm84625 <frankyxm@hotmail.com>
2018-03-27 17:01:52 -0700

    Api.AI integration errorfix v6.3

--- routes/routes.js
+++ routes/routes.js
@@ -51,7 +51,7 @@ rtm.on( 'message', ( event ) => {
         }
     })
     .catch( aiError => { console.log( "Api AI Error: " + aiError ); } )
-    .then( response => response.json() )
+    // .then( response => response.json() )
     .then( response => {
         console.log( response );
         if( response.result.actionIncomplete ) {
