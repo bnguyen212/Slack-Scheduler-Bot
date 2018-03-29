f04096ba7b10aebafe1ad55e5678a9f60bdcade2
fxm84625 <frankyxm@hotmail.com>
2018-03-29 14:01:00 -0700

    Added Automatic Reminder system

--- bin/dailyReminder.js
+++ bin/dailyReminder.js
@@ -8,8 +8,7 @@
 
 // Dependencies
 var SlackClient = require( '@slack/client' );
-    var RTMClient = SlackClient.RTMClient;
-    // var WebClient = SlackClient.WebClient;
+    var WebClient = SlackClient.WebClient;
 
 if( !process.env.SLACK_ACCESS_TOKEN ) { throw new Error( 'process.env.SLACK_ACCESS_TOKEN not found' ); process.exit(1); return; }
 if( !process.env.SLACK_BOT_ACCESS_TOKEN ) { throw new Error( 'process.env.SLACK_BOT_ACCESS_TOKEN not found' ); process.exit(1); return; }
@@ -17,9 +16,7 @@ if( !process.env.SLACK_BOT_ACCESS_TOKEN ) { throw new Error( 'process.env.SLACK_
 var SLACK_ACCESS_TOKEN = process.env.SLACK_ACCESS_TOKEN;
 var SLACK_BOT_ACCESS_TOKEN = process.env.SLACK_BOT_ACCESS_TOKEN;
 
-var rtm = new RTMClient( SLACK_BOT_ACCESS_TOKEN );
-rtm.start();
-// var web = new WebClient( SLACK_BOT_ACCESS_TOKEN );
+var web = new WebClient( SLACK_BOT_ACCESS_TOKEN );
 
 // MongoDB Mongoose Models
 var Models = require( '../models/models.js' );
@@ -29,33 +26,31 @@ function dailyReminder() {
     Reminder.find( {} ).exec()
     .then( foundReminderArray => {
         var today = new Date().getTime();
+        var promiseArray = [];
         // Find all Reminders in Mongoose Database for Today and Tomorrow
         for( var i = 0; i < foundReminderArray.length; i++ ){
-            var timeDiff = foundReminderArray[i].day.getTime() - today;
+            var reminder = foundReminderArray[i];
+            var reminderDate = new Date( reminder.day );
+            var timeDiff = reminderDate.getTime() - today;
             if( timeDiff >= 0 && timeDiff <= 1000*60*60*24*2 ) {
                 // For Each Reminder, Send a Message to that Slack User
                 // If the Reminder is for Today, remove that Reminder from the Database
                 if( timeDiff <= 1000*60*60*24 ) {
-                    // web.chat.postMessage({
-                        // "channel": foundReminderArray[i].slackId,
-                        // "text": "Reminder for Today: " + foundReminderArray[i].subject
-                    // });
-                    rtm.sendMessage( "Reminder for Today: " + foundReminderArray[i].subject, foundReminderArray[i].slackId )
-                    .then( res => console.log( 'Message sent: ', res.ts ) )
-                    .catch( error => console.error );
-                    foundReminderArray[i].remove( removeError => { if( removeError ) console.log( "Reminder Remove Error: " + removeError ); } );
+                    promiseArray.push( web.chat.postMessage({
+                        "channel": reminder.slackId,
+                        "text": "Reminder for Today: " + reminder.subject
+                    }) );
+                    promiseArray.push( reminder.remove() );
                 }
                 else {
-                    // web.chat.postMessage({
-                        // "channel": foundReminderArray[i].slackId,
-                        // "text": "Reminder for Tomorrow: " + foundReminderArray[i].subject
-                    // });
-                    rtm.sendMessage( "Reminder for Tomorrow: " + foundReminderArray[i].subject, foundReminderArray[i].slackId )
-                    .then( res => console.log( 'Message sent: ', res.ts ) )
-                    .catch( error => console.error );
+                    promiseArray.push( web.chat.postMessage({
+                        "channel": reminder.slackId,
+                        "text": "Reminder for Tomorrow: " + reminder.subject
+                    }) );
                 }
             }
         }
+        return Promise.all( promiseArray );
     })
     .then( () => process.exit(0) )
     .catch( error => console.log( error ) );
